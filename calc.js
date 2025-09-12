a65ffb7b40d9d9baa4e9fd1b94a10f9364826012
Ryan Gilbert <ryanjg@bu.edu>
2025-09-12 10:23:20 -0400

    working calc

--- calc.js
+++ calc.js
@@ -1,5 +1,39 @@
 function plus() {
     let i1 = parseFloat(document.getElementById("i1").value);
     let i2 = parseFloat(document.getElementById("i2").value);
-    console.log(i1+i2)
+    document.getElementById("result").innerHTML = i1 + i2;
+}
+
+function minus() {
+    let i1 = parseFloat(document.getElementById("i1").value);
+    let i2 = parseFloat(document.getElementById("i2").value);
+    document.getElementById("result").innerHTML = i1 - i2;
+}
+
+function times() {
+    let i1 = parseFloat(document.getElementById("i1").value);
+    let i2 = parseFloat(document.getElementById("i2").value);
+    document.getElementById("result").innerHTML = i1 * i2;
+}
+
+function divide() {
+    let i1 = parseFloat(document.getElementById("i1").value);
+    let i2 = parseFloat(document.getElementById("i2").value);
+    document.getElementById("result").innerHTML = i1 / i2;
+}
+
+function power() {
+    let i1 = parseFloat(document.getElementById("i1").value);
+    let i2 = parseFloat(document.getElementById("i2").value);
+    let res = i1;
+    for (let i=1; i<i2; i++){
+        res *= i1;
+    }
+    document.getElementById("result").innerHTML = res;
+}
+
+function clear_box() {
+    document.getElementById("i1").value = "";
+    document.getElementById("i2").value = "";
+    document.getElementById("result").innerHTML = "";
 }
